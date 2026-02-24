d127b101f5d970850dfee16614914deebceedcb3
CarolGP <cgpardal@gmail.com>
2026-02-24 19:46:24 +0100

    ejercicio resuleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 
 function App() {
- //Aquí tu código
-}
+    const [name, setName] = useState("Sofía");
+    const [newName, setNewName] = useState('');
 
-export default App
+    const changeName = () => {
+      const text = newName.trim();
+      if(text != ""){
+         setName(text);
+         setNewName('');
+         alert(`El nuevo nombre es: ${text}`);
+    }
+   };
+
+   const handleChange = (e) => {
+      console.log(e.target);
+      setNewName(e.target.value);
+   };
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      changeName();
+   };
+ return(
+<div>
+  <h2>Teacher name:{name}</h2>
+   <ul>
+      <li onClick={() => setName("Data")}>Data</li>
+      <li onClick={() => setName("Reyes")}>Reyes</li>
+      <li onClick={() => setName("Soraya")}>Soraya</li>
+   </ul>
+
+   <button onClick={changeName}>Nuevo nombre</button>
+   <form onSubmit={handleSubmit}>
+      <input 
+        type='text'
+        value={newName}
+        placeholder='add a name'
+        onChange={handleChange}
+      ></input>
+      <button>Cambiar Nombre</button>
+   </form>
+</div>   
+ );
+}
+export default App;
